fcfbc94a004f06bb0bc0c26037c0c1a3354ea7cd
Rajdeep Singh <radewala.2@gmail.com>
2025-03-25 14:43:59 +0530

    contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+import { Container, Heading } from "@/components/ui";
+
+export default function Contact() {
+  return (
+    <Container constrained={true} className="bg-secondary py-5 mt-5 max-w-5">
+      <Heading className="mt-4 mb-2">Contact us</Heading>
+      <p>This is a simple example of a React component.</p>
+    </Container>
+  );
+}
