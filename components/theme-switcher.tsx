9770f01102ad57ba9cfe6f99e786377779a57325
Rajdeep Singh <radewala.2@gmail.com>
2025-03-25 14:45:49 +0530

    theme switch dark to light theme

---
+++ components/theme-switcher.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { IconDeviceDesktop2, IconMoon, IconSun } from "justd-icons";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+
+export function ThemeSwitcher({
+  shape = "square",
+  appearance = "outline",
+  ...props
+}: React.ComponentProps<typeof Button>) {
+  const { theme, setTheme } = useTheme();
+
+  const toggleTheme = () => {
+    const nextTheme =
+      theme === "light" ? "dark" : theme === "dark" ? "system" : "light";
+    setTheme(nextTheme);
+  };
+
+  return (
+    <Button
+      shape={shape}
+      intent={appearance}
+      size="square-petite"
+      aria-label="Switch theme"
+      onPress={toggleTheme}
+      {...props}
+    >
+      {theme === "light" ? (
+        <IconSun />
+      ) : theme === "dark" ? (
+        <IconMoon />
+      ) : (
+        <IconDeviceDesktop2 />
+      )}
+    </Button>
+  );
+}
